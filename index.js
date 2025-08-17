7354202ae640978bfee94fb7532ba4e6d8aa7368
Masrafe Efte <eftemasrafe@gmail.com>
2025-08-17 19:58:08 +0600

    deployed server to vercel

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const app = express();
-const port = process.env.PORT || 5000;
+const jwt = require('jsonwebtoken');
+const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
@@ -32,19 +33,58 @@ async function run() {
 
     const jobsCollection = client.db('careerCode').collection('jobs');
     const applicationsCollection = client.db('careerCode').collection('applications');
+   
+    //jwt token related api
+    app.post('/jwt',async(req,res)=>{
+      const {email} = req.body;
+      const user = { email }
+      const token = jwt.sign(user,'secret',{ expiresIn: '1h' });
+      res.send({ token })
+    })
+
     //jobs api
     app.get('/jobs', async (req,res) =>{
+
+      const email = req.query.email;
+      const query = {};
+      if(email){
+        query.hr_email = email;
+      }
       const cursor = jobsCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+   //could be done but should not be done
+  //  app.get('/jobsByEmailAddress',async(req,res)=>{
+  //   const email = req.query.email;
+  //   const query = {hr_email:email}
+  //   const result = await jobsCollection.find(query).toArray();
+  //   res.send(result);
+  //  })
+
     app.get('/jobs/:id', async(req,res) =>{
       const id = req.params.id;
       const query = { _id: new ObjectId(id)}
       const result = await jobsCollection.findOne(query);
+      res.send(result);
+    });
+
+    //app.get('/applications/:id',()=>{})
+      app.get('/applications/job/:job_id',async (req,res) =>{
+        const job_id = req.params.job_id;
+        const query = {jobId: job_id}
+        const result = await applicationsCollection.find(query).toArray();
+        res.send(result);
+      } )
+
+    app.post('/jobs', async(req,res)=>{
+      const newJob = req.body;
+      console.log(newJob);
+      const result = await jobsCollection.insertOne(newJob);
       res.send(result)
-    } )
+      
+    })
  
     //job applications related apis
 
@@ -70,6 +110,18 @@ async function run() {
       const application = req.body;
       console.log(application);
       const result = await applicationsCollection.insertOne(application);
+      res.send(result);
+    })
+
+    app.patch('/applications/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          status:req.body.status
+        }
+      }
+      const result = await applicationsCollection.updateOne(filter,updatedDoc)
       res.send(result);
     })
      
